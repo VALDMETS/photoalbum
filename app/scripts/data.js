70525ee9259a9021ad65a23b2bd73d2de75f51ad
Ben Valdmets-Harris <benvaldmets@gmail.com>
2016-07-01 14:56:36 -0500

    added captions

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -1,42 +1,71 @@
 var albumList = [{
     albumName: 'Dark Side of the Moon',
-    photos: ['file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/dark01.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/dark02.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/dark03.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/dark04.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/dark05.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/dark06.jpg']
+    photos: [{source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/dark01.jpg',
+              caption: 'In all its majesty'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/dark02.jpg',
+              caption: 'Black and White Chic'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/dark03.jpg',
+              caption: 'At the barber'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/dark04.jpg',
+              caption: 'Live at Stonehenge, 1977'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/dark05.jpg',
+              caption: 'Live in Moscow, 1983'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/dark06.jpg',
+              caption: 'Beach Vacation'}]
 }, {
     albumName: 'Intense Space',
-    photos: ['file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/space01.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/space02.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/space03.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/space04.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/space05.jpg']
+    photos: [{source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/space02.jpg',
+              caption: 'Gnarly Gnebula'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/space01.jpg',
+              caption: 'Quool Quasar'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/space03.jpg',
+              caption: 'Space is big'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/space04.jpg',
+              caption: 'People are small'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/space05.jpg',
+              caption: 'Space Fantasy'}]
 }, {
     albumName: 'The Grand Wazoo',
-    photos: ['file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/wazoo01.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/wazoo02.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/wazoo03.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/wazoo04.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/wazoo05.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/wazoo06.jpg']
+    photos: [{source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/wazoo01.jpg',
+              caption: 'The Grand Album Cover'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/wazoo02.jpg',
+              caption: 'Use some shampoo, Frank'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/wazoo03.jpg',
+              caption: 'Live in Austin, 1977'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/wazoo04.jpg',
+              caption: 'Don\'t get any ideas'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/wazoo05.jpg',
+              caption: 'The Mothers of Invention'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/wazoo06.jpg',
+              caption: 'Sweet Shredding'}]
 }, {
     albumName: 'Hot Hot Rods',
-    photos: ['file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/car01.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/car02.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/car03.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/car04.jpg']
+    photos: [{source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/car01.jpg',
+              caption: 'Still drives! 18k OBO'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/car02.jpg',
+              caption: 'Minor alignment issues'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/car03.jpg',
+              caption: 'Some body work needed'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/car04.jpg',
+              caption: 'Great value!'}]
 }, {
     albumName: 'Boring Ocean',
-    photos: ['file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/ocean01.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/ocean02.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/ocean03.jpg',]
+    photos: [{source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/ocean01.jpg',
+              caption: 'Very boring'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/ocean02.jpg',
+              caption: 'Yawn'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/ocean03.jpg',
+              caption: 'Honestly dull and fake'}]
 }, {
     albumName: 'All Bricks',
-    photos: ['file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/brick01.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/brick02.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/brick03.png',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/brick04.jpg',
-             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/brick05.jpg']
+    photos: [{source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/brick01.jpg',
+              caption: 'Red brick'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/brick02.jpg',
+              caption: 'White brick'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/brick03.png',
+              caption: 'Zany brick'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/brick04.jpg',
+              caption: 'Live at Brick'},
+             {source:'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/brick05.jpg',
+              caption: 'Ball don\'t lie'}]
 }];
--- dist/main.js
+++ dist/main.js
@@ -1,65 +1,11 @@
-var albumList = [{
-    albumName: 'Dark Side of the Moon',
-    // albumTag:  'darkside',
-    photos: ['https://hronrad.files.wordpress.com/2013/03/album_the_dark_side_of_the_moon_hd_by_rafaelbacelar-d562dlk.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg']
-}, {
-    albumName: 'Ho Ho Hee Hee',
-    // albumTag:  'hoho',
-    photos: ['http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg']
-}, {
-    albumName: 'The Grand Wazoo',
-    // albumTag:  'wazoo',
-    photos: ['http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg']
-}, {
-    albumName: 'Dark Side of the Moon',
-    // albumTag:  'wowzers',
-    photos: ['http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg']
-}, {
-    albumName: 'Ho Ho Hee Hee',
-    // albumTag:  'shrimp',
-    photos: ['http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg']
-}, {
-    albumName: 'The Grand Wazoo',
-    // albumTag:  'blurgh',
-    photos: ['http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
-             'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg']
-}];
 var $albumBox = $('.album-list');
 var $photoBox = $('.photo-list');
+var returnPageOk = 0;
 
 window.location.hash = 'default';
 
 albumList.forEach(function(album, i) {
-    var totalInsert = '<a class="album-button" href="#' + album.albumName+ '"><img src="' + album.photos[0] + '">' + album.albumName + '</a>';
+    var totalInsert = '<a class="album-button" href="#' + album.albumName+ '"><img src="' + album.photos[0].source + '">' + album.albumName + '</a>';
     $albumBox.append(totalInsert);
 });
 
@@ -71,6 +17,7 @@ $(document).ready(function() {
         var picsToShow = albumList.filter(function(album, i) {
             return albumList[i].albumName === albumID;
         });
+        console.log(picsToShow);
         picsToShow = picsToShow[0];
         $albumBox.addClass('side-albums');
         $('.album-button').addClass('side-bar');
@@ -78,21 +25,33 @@ $(document).ready(function() {
         currentAlbumName = location.hash.slice(1);
         document.querySelector('.photo-list').innerHTML = '<h2>'+ currentAlbumName + '</h2>';
         picsToShow.photos.forEach(function(item, i, arr) {
-            totalInsert = '<div class="album-button"><img src="' + item + '">Picture ' + (i + 1) + ' </div>';
+            totalInsert = '<div class="album-button"><img src="' + item.source + '">' + item.caption + ' </div>';
+            console.log(totalInsert);
             $photoBox.append(totalInsert);
         });
         photoTrigger = document.querySelectorAll('div');
         photoTrigger.forEach(function(button){
             $(button).click(function(e){
-            $photoBox.addClass('pic-focus');
-            $('img').addClass('hide');
-            $('div').addClass('hide');
-            document.querySelector('h2').innerText = 'Back to Album';
-            $(e.target).removeClass('hide');
-            $(e.target).addClass('highlight');
+                $photoBox.addClass('pic-focus');
+                $('img').addClass('hide');
+                $('div').addClass('hide');
+                document.querySelector('h2').innerText = 'Back to Album';
+                $(e.target).removeClass('hide');
+                $(e.target).addClass('highlight');
+                $('html,body').scrollTop(0);
+                returnPageOk = 1;
             });
         });
-
+        $('h2').click(function() {
+            if (returnPageOk === 1) {
+                $photoBox.removeClass('pic-focus');
+                console.log(document.querySelector('.photo-list div img'));
+                $(document.querySelectorAll('.photo-list div img')).removeClass('hide');
+                $('div').removeClass('hide');
+                document.querySelector('h2').innerText = currentAlbumName;
+                $('img').removeClass('highlight');
+                returnPageOk = 0;
+            }
+        });
     });
-
 });
