3bccd70d37f049273f9b0a807d5adfc928ac585b
Ben Valdmets-Harris <benvaldmets@gmail.com>
2016-06-30 21:37:37 -0500

    finally got the picture zoom working

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,5 @@
-var albumList = [
-  { albumName: 'Dark Side of the Moon',
+var albumList = [{
+    albumName: 'Dark Side of the Moon',
     albumTag:  'darkside',
     photos: ['http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
@@ -7,8 +7,8 @@ var albumList = [
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg']
-  },
-  { albumName: 'Ho Ho Hee Hee',
+}, {
+    albumName: 'Ho Ho Hee Hee',
     albumTag:  'hoho',
     photos: ['http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
@@ -16,8 +16,8 @@ var albumList = [
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg']
-  },
-  { albumName: 'The Grand Wazoo',
+}, {
+    albumName: 'The Grand Wazoo',
     albumTag:  'wazoo',
     photos: ['http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
@@ -25,8 +25,8 @@ var albumList = [
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg']
-  },
-  { albumName: 'Dark Side of the Moon',
+}, {
+    albumName: 'Dark Side of the Moon',
     albumTag:  'wowzers',
     photos: ['http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
@@ -34,8 +34,8 @@ var albumList = [
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg']
-  },
-  { albumName: 'Ho Ho Hee Hee',
+}, {
+    albumName: 'Ho Ho Hee Hee',
     albumTag:  'shrimp',
     photos: ['http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
@@ -43,8 +43,8 @@ var albumList = [
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg']
-  },
-  { albumName: 'The Grand Wazoo',
+}, {
+    albumName: 'The Grand Wazoo',
     albumTag:  'blurgh',
     photos: ['http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
@@ -52,21 +52,51 @@ var albumList = [
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg',
              'http://lisabuffaloe.com/wp-content/uploads/2015/03/Blah.jpg']
-  }
-];
+}];
 var $albumBox = $('.album-list');
-console.log(albumList);
+var $photoBox = $('.photo-list');
 
-albumList.forEach(function(album,i){
-  var totalInsert = '<a class="album-button" href="#' + album.albumTag + '"><img src="' + album.photos[0] + '">' + album.albumName + '</a>';
-  console.log(totalInsert);
-  $albumBox.append(totalInsert);
+window.location.hash = 'default';
+
+albumList.forEach(function(album, i) {
+    var totalInsert = '<a class="album-button" href="#' + album.albumTag + '"><img src="' + album.photos[0] + '">' + album.albumName + '</a>';
+    $albumBox.append(totalInsert);
 });
 
-window.addEventListener('hashchange',function(){
-    $albumBox.addClass('side-albums');
-    $('.album-button').addClass('side-bar');
-    $('img').addClass('hide');
-    currentAlbumName = location.hash.slice(1);
-    console.log(location.hash);
+$(document).ready(function() {
+    window.addEventListener('hashchange', function() {
+
+        var albumID = location.hash.slice(1);
+        var picsToShow = albumList.filter(function(album, i) {
+            return albumList[i].albumTag === albumID;
+        });
+        picsToShow = picsToShow[0];
+        $albumBox.addClass('side-albums');
+        $('.album-button').addClass('side-bar');
+        $('img').addClass('hide');
+        currentAlbumName = location.hash.slice(1);
+        document.querySelector('.photo-list').innerHTML = '<h2>My Pictures</h2>';
+        picsToShow.photos.forEach(function(item, i, arr) {
+            totalInsert = '<div class="album-button"><img src="' + item + '">Picture #' + (i + 1) + ' </div>';
+            $photoBox.append(totalInsert);
+        });
+        console.log($('div'));
+        photoTrigger = document.querySelectorAll('div');
+        photoTrigger.forEach(function(button){
+            $(button).click(function(e){
+            $photoBox.addClass('pic-focus');
+            $('img').addClass('hide');
+            $('div').addClass('hide');
+            // $('header').addClass('hide');
+            // $albumBox.addClass('shrink');
+            document.querySelector('h2').innerText = '';
+            $(e.target).removeClass('hide');
+            $(e.target).addClass('highlight');
+            console.log(e.target);
+            });
+        });
+        console.log(photoTrigger);
+
+    });
+
 });
