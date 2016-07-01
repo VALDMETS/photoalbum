9ad7eb8ef65a39770ba310719dd9b3aae5e12114
Ben Valdmets-Harris <benvaldmets@gmail.com>
2016-07-01 14:05:42 -0500

    images full

---
+++ app/scripts/data.js
@@ -0,0 +1,42 @@
+var albumList = [{
+    albumName: 'Dark Side of the Moon',
+    photos: ['file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/dark01.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/dark02.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/dark03.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/dark04.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/dark05.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/dark06.jpg']
+}, {
+    albumName: 'Intense Space',
+    photos: ['file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/space01.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/space02.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/space03.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/space04.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/space05.jpg']
+}, {
+    albumName: 'The Grand Wazoo',
+    photos: ['file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/wazoo01.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/wazoo02.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/wazoo03.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/wazoo04.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/wazoo05.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/wazoo06.jpg']
+}, {
+    albumName: 'Hot Hot Rods',
+    photos: ['file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/car01.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/car02.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/car03.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/car04.jpg']
+}, {
+    albumName: 'Boring Ocean',
+    photos: ['file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/ocean01.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/ocean02.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/ocean03.jpg',]
+}, {
+    albumName: 'All Bricks',
+    photos: ['file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/brick01.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/brick02.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/brick03.png',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/brick04.jpg',
+             'file://localhost/users/Ben/tiy/day-14/assignments/album/app/images/brick05.jpg']
+}];
